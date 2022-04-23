fb9f62b45251208fb071f3f9e722a38e061529b9
TheGreatKAN <95988106+TheGreatKAN@users.noreply.github.com>
2022-04-22 21:03:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -400,7 +400,7 @@ function App() {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary)",
+              color: "var(--footer)",
             }}
           >
             Please make sure you are connected to the right network (
@@ -411,7 +411,7 @@ function App() {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary)",
+              color: "var(--footer)",
             }}
           >
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
